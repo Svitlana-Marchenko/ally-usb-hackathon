0401b1b28077c12e0cdbf3844582e0fa7fcf4b73
Olha Horban <horbanolha64@gmail.com>
2024-05-11 19:00:47 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from '@nextui-org/button';
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+        <Button>Click me</Button>
+    </main>
+  );
+}
